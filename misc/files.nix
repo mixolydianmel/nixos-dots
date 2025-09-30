e92d9502b4d055bc0a6925f12b8b245ef8abaab9
Melody Henrich <mixolydianmel@proton.me>
2025-09-30 11:14:44 -0400

    Updated keyboard config to correct errors in keys and naming

--- misc/files.nix
+++ misc/files.nix
@@ -6,7 +6,6 @@
   # Link xdg config files (~/.config/<file>)
   xdg.configFile = {
     "niri/config.kdl".source = ../configs/niri/config.kdl;
-    "xkb/shaw-jafl.xkm".source = ../configs/xkb/shaw-jafl.xkm;
-    "xkb/shaw-jafl.xkb".source = ../configs/xkb/shaw-jafl.xkb;
+    "xkb/shaw-imperial.xkb".source = ../configs/xkb/shaw-imperial.xkb;
   };
 }
